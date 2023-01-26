2d728fda48c375016168511d156272529c652061
Chris_tAt <christati1205@gmail.com>
2023-01-25 22:45:51 -0500

    el proyecto va a paso lento, apenas vamos en la cabecera de la aplicacion web, esperamos poder colocarle algunos diseños restantes. se creo tengo dificultad para agregar una imagene a la cabecera. se crearon diferentes componentes (debo intentar poner mas componentes para que un componente no se vea tan saturado de codigos)

---
+++ proyectomovie1/src/App.js
@@ -0,0 +1,22 @@
+
+
+import SaludTotal from './Componentes/SaludTotal';
+
+
+
+function  App () {
+  return (
+   <div>
+    <header>
+      <section>
+        <hr/>
+        <SaludTotal/>
+       
+      </section>
+    </header>
+   </div>
+
+  );
+}
+
+export default App;
